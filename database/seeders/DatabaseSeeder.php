addde37108ac623988aa0d1cba15c69b582222dc
Aji <ajidianpermana2@gmail.com>
2024-06-02 23:36:13 +0700

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::create([
+            'name'     => 'Atana',
+            'email'    => 'atanaalfiihza@gmail.com',
+            'password' => bcrypt('Atana12345*'),
         ]);
     }
 }
